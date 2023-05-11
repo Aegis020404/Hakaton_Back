9e5b08a0eb466ead517981fcbb80e8c3496c87d0
lomys <aegis020404@gmail.com>
2023-05-12 02:11:55 +0300

    d

--- config/server.js
+++ config/server.js
@@ -1,6 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  // host: env('HOST', '0.0.0.0'),
+  // port: env.int('PORT', 1337),
+  "users-permissions": {
+    jwtSecret: env("JWT_SECRET", "defaultSecret"),
+    // Other configuration options...
+  },
   app: {
     // keys: env.array('APP_KEYS'),
     keys: ['myKeyA', 'myKeyB'],
